8ed9c2806e3fb0e1f83f134f19101d4ea27c3a38
Ana Belén Hernández <anahernandezfit@gmail.com>
2025-06-18 11:55:40 +0200

    Add simple Spring Boot backend API project files and Dockerfile

---
+++ src/main/java/com/anabelen/simple_backend_api/SimpleBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.anabelen.simple_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anabelen/simple_backend_api/SimpleBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabelen.simple_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
